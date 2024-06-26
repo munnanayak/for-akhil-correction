6d9cebf3090d98766c0cf74ee4b5f4aa272802e0
hu-lk <56403979+hu-lk@users.noreply.github.com>
2024-06-26 13:07:21 +0530

    resolved routes issues

--- src/App.js
+++ src/App.js
@@ -1,50 +1,82 @@
-import React from 'react';
-// components
-import Header from './components/Header';
-import Footer from './components/Footer';
-// pages
-import Home from './pages/Home';
-import About from './pages/About';
-import Restaurant from './pages/Restaurant';
-import RoomDetails from './pages/RoomDetails';
+// import React from "react";
+// // components
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// // pages
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Restaurant from "./pages/Restaurant";
+// import RoomDetails from "./pages/RoomDetails";
 
-// react router
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import PreHeader from './components/PreHeader';
-import Gallery from './pages/Gallery';
+// // react router
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import PreHeader from "./components/PreHeader";
+// import Gallery from "./pages/Gallery";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/room/:id',
-    element: <RoomDetails />,
-  },
-  
-  {
-    path: '/about', // 
-    element: <About />,
-  },
-  {
-    path: '/restaurant', // 
-    element: <Restaurant />,
-  },
-  {
-    path: '/gellery', // 
-    element: <Gallery />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/room/:id",
+//     element: <RoomDetails />,
+//   },
+
+//   {
+//     path: "/about", //
+//     element: <About />,
+//   },
+//   {
+//     path: "/restaurant", //
+//     element: <Restaurant />,
+//   },
+//   {
+//     path: "/gellery", //
+//     element: <Gallery />,
+//   },
+// ]);
+
+// const App = () => {
+//   return (
+//     <div>
+//       <PreHeader />
+//       <Header />
+//       <RouterProvider router={router} />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PreHeader from "./components/PreHeader";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Restaurant from "./pages/Restaurant";
+import Gallery from "./pages/Gallery";
+import RoomDetails from "./pages/RoomDetails";
+import SampleRoom from "./pages/sampleroom";
 
 const App = () => {
   return (
-    <div>
-      <PreHeader/>
+    <Router>
+      <PreHeader />
       <Header />
-      <RouterProvider router={router} />
+      {/* Define your routes here using Routes component */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/restaurant" element={<Restaurant />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/room/:id" element={<RoomDetails />} />
+        <Route path="/sampleroom" element={<SampleRoom />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
